90a411d3ad34cb01ee3e3eace60741dbd7e9ac0c
Lavdimi <lavdim.pireva@studentet.uni-pr.edu>
2019-05-23 00:21:36 +0200

    FirstCommit

--- dashboard/airplane.php
+++ dashboard/airplane.php
@@ -176,103 +176,65 @@
                     <div class="col-md-8">
                         <div class="card">
                             <div class="header">
-                                <h4 class="title">Edit User</h4>
+                                <h4 class="title">Edit Airplane</h4>
                             </div>
                             <div class="content">
                                 <form>
-                                    <div class="row">
+                                    <div class="row" style="margin-left:25%">
                                         <div class="col-md-5">
                                             <div class="form-group" style="width: 250px;">
-                                                <label>First Name</label>
-                                                <input type="text" class="form-control"  placeholder="Company" value="Creative Code Inc.">
+                                                <label>Name</label>
+                                                <input type="text" class="form-control"  placeholder="Name">
                                             </div>
                                         </div>
-                                        <div class="col-md-3" style="width: 300px;">
-                                            <div class="form-group">
-                                                <label>LAST NAME</label>
-                                                <input type="text" class="form-control" placeholder="Username" value="michael23">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3" style="width: 30px;">
-                                        <label style="padding-right:5px"> Gender </label>
-                                        <br>
-                                        <select style="padding:7.5px;">
-                                              <option class="form-group" value="M">M</option>
-                                              <option value="F">F</option>
-                                        </select> 
-                                        </div>
-
                                     </div>
-
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label>Password</label>
-                                                <input type="password" placeholder="password" class="form-control">
+                                        <div class="row">
+                                        <div class="col-md-3">
+                                            <div class="form-group" style="width:120px;">
+                                                <label>Year Of Produce</label>
+                                                <input type="number" class="form-control" placeholder="Year" min="1800" style="width: 120px; padding-right:20">
                                             </div>
                                         </div>
-                                        <div class="col-md-6">
+                                        <div class="col-md-3">
+                                        <label style="padding-right:5px ; padding-left:-10px;"> Seats </label>
+                                        <input type="number" class="form-control" placeholder="Seats">
+                                        </div>
+                                    
+                                        <div class="col-md-3">
                                             <div class="form-group">
-                                                <label>Birthday</label>l
-                                                <input type="date" class="form-control" placeholder="Birthday">
+                                                <label>Fuel Capacity</label>
+                                                <input type="number" placeholder="Fuel Capacity" class="form-control" style="width: 150px;">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6" style="width:150px;">
+                                            <div class="form-group">
+                                                <label>Max Speed</label>l
+                                                <input type="number" class="form-control" placeholder="Max speed" style="width: 150px;">
                                             </div>
                                         </div>
                                     </div>
                                     
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Address</label>
-                                                <input type="text" class="form-control" placeholder="Home Address" width="20">
-                                            </div>
-                                        </div>
-                                    </div>
-                                                                         
-
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>City</label>
-                                                <input type="text" class="form-control" placeholder="City" value="Mike">
-                                            </div>
-                                        </div>
-                                       
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>Country</label>
-                                                <input type="text" class="form-control" placeholder="Country" value="Andrew">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>Postal Code</label>
-                                                <input type="number" class="form-control" placeholder="ZIP Code">
-                                            </div>
-                                        </div>
-                                     </div>
-                                     <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>Phone number</label>
-                                                <input type="text" class="form-control" placeholder=" +000-00-000-000">
-                                            </div>
-                                        </div>
+                                    <div class="row" style="margin-left:23.5%">
                                        
                                         <div class="col-md-4">
                                             <div class="form-group">
                                                 <label>Created at</label>
-                                                <input type="datetime" class="form-control" placeholder="                __/__/____">
+                                                <input type="text" class="form-control" placeholder="          __/__/____">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="form-group">
                                                 <label>Updated at</label>
-                                                <input type="datetime" class="form-control" placeholder="                __/__/____">
+                                                <input type="text" class="form-control" placeholder="          __/__/____">
                                             </div>
                                         </div>
                                      </div>
+                                     <div class="row" style="padding-left:18px; padding-right:18px; padding-bottom:10px">
+                                     <label>AdditionalDesc</label>
+                                     <textarea rows="5" class="form-control" placeholder="Here can be your description"></textarea>   
+                                     </div>
 
-                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update User</button>
+                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update Flights</button>
                                     <div class="clearfix"></div>
                                 </form>
                             </div>
--- dashboard/flights.php
+++ dashboard/flights.php
@@ -176,29 +176,29 @@
                     <div class="col-md-8">
                         <div class="card">
                             <div class="header">
-                                <h4 class="title">Edit User</h4>
+                                <h4 class="title">Edit Flights</h4>
                             </div>
                             <div class="content">
                                 <form>
                                     <div class="row">
                                         <div class="col-md-5">
                                             <div class="form-group" style="width: 250px;">
-                                                <label>First Name</label>
-                                                <input type="text" class="form-control"  placeholder="Company" value="Creative Code Inc.">
+                                                <label>From City</label>
+                                                <input type="number" class="form-control"  placeholder="cityId" >
                                             </div>
                                         </div>
                                         <div class="col-md-3" style="width: 300px;">
                                             <div class="form-group">
-                                                <label>LAST NAME</label>
-                                                <input type="text" class="form-control" placeholder="Username" value="michael23">
+                                                <label>To City</label>
+                                                <input type="text" class="form-control" placeholder="To City">
                                             </div>
                                         </div>
                                         <div class="col-md-3" style="width: 30px;">
-                                        <label style="padding-right:5px"> Gender </label>
+                                        <label style="padding-right:5px"> Available </label>
                                         <br>
                                         <select style="padding:7.5px;">
-                                              <option class="form-group" value="M">M</option>
-                                              <option value="F">F</option>
+                                              <option class="form-group" value="True">True</option>
+                                              <option value="False">False</option>
                                         </select> 
                                         </div>
 
@@ -207,72 +207,50 @@
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label>Password</label>
-                                                <input type="password" placeholder="password" class="form-control">
+                                                <label>Airplane</label>
+                                                <input type="text" placeholder="Airplane" class="form-control">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label>Birthday</label>l
-                                                <input type="date" class="form-control" placeholder="Birthday">
+                                                <label>CheckIn</label>l
+                                                <input type="datetime" class="form-control" placeholder="__/__/____">
                                             </div>
                                         </div>
                                     </div>
-                                    
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Address</label>
-                                                <input type="text" class="form-control" placeholder="Home Address" width="20">
-                                            </div>
-                                        </div>
-                                    </div>
-                                                                         
-
                                     <div class="row">
                                         <div class="col-md-4">
                                             <div class="form-group">
-                                                <label>City</label>
-                                                <input type="text" class="form-control" placeholder="City" value="Mike">
+                                                <label>Price</label>
+                                                <input type="number" class="form-control" placeholder="Price">
                                             </div>
+                                        </div>
+                                       <div class="col-md-3" style="width: 30px;">
+                                       <label> isSale </label>
+                                        <br>
+                                        <select style="padding:7.5px;">
+                                              <option class="form-group" value="True">True</option>
+                                              <option value="False">False</option>
+                                        </select> 
                                         </div>
                                        
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>Country</label>
-                                                <input type="text" class="form-control" placeholder="Country" value="Andrew">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label>Postal Code</label>
-                                                <input type="number" class="form-control" placeholder="ZIP Code">
-                                            </div>
-                                        </div>
                                      </div>
                                      <div class="row">
                                         <div class="col-md-4">
                                             <div class="form-group">
-                                                <label>Phone number</label>
-                                                <input type="text" class="form-control" placeholder=" +000-00-000-000">
-                                            </div>
-                                        </div>
-                                       
-                                        <div class="col-md-4">
-                                            <div class="form-group">
                                                 <label>Created at</label>
-                                                <input type="datetime" class="form-control" placeholder="                __/__/____">
+                                                <input type="text" class="form-control" placeholder="                __/__/____">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="form-group">
                                                 <label>Updated at</label>
-                                                <input type="datetime" class="form-control" placeholder="                __/__/____">
+                                                <input type="text" class="form-control" placeholder="                __/__/____">
                                             </div>
                                         </div>
                                      </div>
 
-                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update User</button>
+                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update Flights</button>
                                     <div class="clearfix"></div>
                                 </form>
                             </div>

fd54c1c489a8bb587b634ce81044d3d90b989203
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-24 02:01:57 +0200

    PHP files to  components

---
+++ dashboard/components/flights-table.php
@@ -0,0 +1,89 @@
+						<div class="card">
+							<div class="header">
+								<h4 class="title">Number of airplanes:5</h4>
+							</div>
+							<div class="content table-responsive table-full-width">
+								<table class="table table-hover table-striped">
+									<thead>
+										<th>Image</th>
+										<th>From city </th>
+										<th>To city </th>
+										<th>Plane id </th>
+										<th>Price</th>
+										<th>Is sale</th>
+										<th>Check in</th>
+										<th>Created at</th>
+										<th>Updated at</th>
+									</thead>
+									<tbody>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+										<tr>
+											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+											<td>Prishtine</td>
+											<td>Wiene</td>
+											<td style="padding-left:25px;">1</td>
+											<td>100</td>
+											<td>True</td>
+											<td>01/01/2000 01:01:01</td>
+											<td>01/01/2000</td>
+											<th>01/01/2000</th>
+										</tr>
+									</tbody>
+								</table>
+
+							</div>
+						</div>
--- dashboard/components/sidebar.php
+++ dashboard/components/sidebar.php
@@ -1,61 +1,60 @@
+<div class="sidebar" data-color="blue" data-image="assets/img/sidebar-5.jpg">
 
- <div class="sidebar" data-color="blue" data-image="assets/img/sidebar-5.jpg">
-
-    <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+	<!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
 
 
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="http://www.creative-tim.com" class="simple-text">
-                    Creative Tim
-                </a>
-            </div>
+	<div class="sidebar-wrapper">
+		<div class="logo">
+			<a href="http://www.creative-tim.com" class="simple-text">
+				Creative Tim
+			</a>
+		</div>
 
-            <ul class="nav">
-                <li>
-                    <a href="dashboard.php">
-                        <i class="pe-7s-graph"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li class="active">
-                    <a href="user.php">
-                        <i class="pe-7s-user"></i>
-                        <p>User Profile</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="users-table.php">
-                        <i class="pe-7s-note2"></i>
-                        <p>Users List</p>
-                    </a>
-                </li>
-                <li>
-                  <a href="airplane-table.php">
-                      <i class="pe-7s-note2"></i>
-                      <p>Airplanes List</p>
-                  </a>
-              </li>
-              <li>
-               <a href="flights-table.php">
-                   <i class="pe-7s-note2"></i>
-                   <p>Flights List</p>
-               </a>
-           </li>
-           <li>
-               <a href="flights-table.php">
-                   <i class="pe-7s-note2"></i>
-                   <p>Booked</p>
-               </a>
-           </li>
-                
-              
-                <li>
-                    <a href="maps.php">
-                        <i class="pe-7s-map-marker"></i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-            </ul>
-    	</div>
-    </div>
+		<ul class="nav">
+			<li>
+				<a href="dashboard.php">
+					<i class="pe-7s-graph"></i>
+					<p>Dashboard</p>
+				</a>
+			</li>
+			<li class="active">
+				<a href="user.php">
+					<i class="pe-7s-user"></i>
+					<p>User Profile</p>
+				</a>
+			</li>
+			<li>
+				<a href="users-table.php">
+					<i class="pe-7s-note2"></i>
+					<p>Users List</p>
+				</a>
+			</li>
+			<li>
+				<a href="airplane-table.php">
+					<i class="pe-7s-note2"></i>
+					<p>Airplanes List</p>
+				</a>
+			</li>
+			<li>
+				<a href="flights-table.php">
+					<i class="pe-7s-note2"></i>
+					<p>Flights List</p>
+				</a>
+			</li>
+			<li>
+				<a href="booked-table.php">
+					<i class="pe-7s-note2"></i>
+					<p>Booked</p>
+				</a>
+			</li>
+
+
+			<li>
+				<a href="maps.php">
+					<i class="pe-7s-map-marker"></i>
+					<p>Maps</p>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>

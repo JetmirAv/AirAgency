dc1c7364c8d574b8081f995337753b82b61f8c92
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-25 11:27:28 +0200

    Data from database

--- dashboard/components/airplane-table.php
+++ dashboard/components/airplane-table.php
@@ -1,82 +1,70 @@
-						<div class="card">
-							<div class="header">
-								<h4 class="title">Number of airplanes:5</h4>
-							</div>
-							<div class="content table-responsive table-full-width">
-								<table class="table table-hover table-striped">
-									<thead>
-										<th>Image</th>
-										<th>Name</th>
-										<th>Seats </th>
-										<th>Year of produced </th>
-										<th>Fuel Capacity </th>
-										<th>Max Speed</th>
-										<th>Created at </th>
-										<th>Updated at </th>
-									</thead>
-									<tbody>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-										<tr>
-											<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-											<td>Boeing 747</td>
-											<td style="padding-left:25px;">747</td>
-											<td style="padding-left:40px">2000</td>
-											<td>920 km/h</td>
-											<td>48,445 gallons</td>
-											<td>01/01/2000</td>
-											<th>01/01/2000</th>
-										</tr>
-									</tbody>
-								</table>
+<div class="card">
 
-							</div>
-						</div>
+
+	<?php include "../databaseConfig.php"?>
+	<?php
+	if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+	$start_from = ($page-1) * 8;
+
+	$sql = "select concat('assets/img/airplanes/',img) as image,name, seats, yearOfProd,fuelCapacity,maxspeed,createdAt,updatedAt from airplane limit $start_from,8";
+	$rs_result = $conn->query($sql);	
+	$count = $rs_result->rowCount();
+		?>
+
+	<div class="header">
+		<h4 class="title">Number of airplanes:<?php print $count ?></h4>
+	</div>
+	<div class="content table-responsive table-full-width">
+		<?php	
+	   echo '<table class="table table-hover table-striped">
+		    <thead>
+			 <th>Image</th>
+			 <th>Name</th>
+			 <th>Seats </th>
+			 <th>Year of produced </th>
+			 <th>Fuel Capacity </th>
+			 <th>Max Speed</th>
+			 <th>Created at </th>
+			 <th>Updated at </th>
+		    </thead>
+		 <tbody>';
+
+
+	foreach($rs_result as $row){
+			echo' <tr>
+				<td style="padding:2px ; padding-left:10px"><img src="'.$row["image"].'" width=35; height=35; style="border-radius:50% ; padding:0px;"> </td></td>
+				<td>'.$row["name"].'</td>
+
+				<td style="padding-left:25px;">'.$row["seats"].'</td>
+				<td style="padding-left:40px">'.$row["yearOfProd"].'</td>
+				<td>'.$row["fuelCapacity"].'</td>
+				<td>'.$row["maxspeed"].'</td>
+				<td>'.$row["createdAt"].'</td>
+				<th>'.$row["updatedAt"].'</th>
+			</tr>';
+	}
+			
+		?>
+		</tbody>
+		</table>
+
+		<?php
+		$sql = "SELECT COUNT(id) AS total FROM airplane";
+		$result = $conn->query($sql);
+		$row = $result->fetch();
+		
+		$a=$row['total']/8;
+		
+		$total_pages = ceil($a); // calculate total pages with results
+        echo     "<div style='text-align: center'>";
+		for ($i=1; $i<=$total_pages; $i++)   // print links for all pages
+			{ 
+			echo "<a href='/github/AirAgency/dashboard/airplane-table.php?page=".$i."' ";
+			echo "style='margin-left:10px;'>"."      ".$i."</a> ";
+		    }; 
+		     
+		echo "</div>" 
+?>
+
+	</div>
+</div>

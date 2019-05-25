6951ea0394e2fa7d58dae587c8876ef4eafd1fc5
Hajrije <44509293+Hajrije@users.noreply.github.com>
2019-05-25 22:57:37 +0200

    updateAirplane Form->DB

---
+++ dashboard/components/dashboard/updateAirplane.php
@@ -0,0 +1,36 @@
+<?php
+$servername = 'localhost';
+$username = "root";
+$password = "";
+$database = 'airagency';
+//include "../databaseConfig.php";
+
+try{
+   
+    $conn = new PDO("mysql:host=localhost;dbname=airagency", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $name = $_POST['name'];
+    $yearOfProd = $_POST['yearOfProd'];
+    $seats = $_POST['seats'];
+    $fuelCapacity = $_POST['fuelCapacity'];
+    $maxspeed = $_POST['maxspeed'];
+    $additionalDesc = $_POST['additionalDesc'];
+    $img = $_POST['img'];
+    
+    
+    $updatePlane = "update airplane set name = :name, yearOfProd = :yearOfProd, seats = :seats, fuelCapacity = :fuelCapacity, maxspeed = :maxspeed, additionalDesc = :additionalDesc, img = :img where id = 105;";
+    $updateStm = $conn->prepare($updatePlane);
+    $pdoExec = $updateStm->execute(array(":name"=>$name,":yearOfProd"=>$yearOfProd,":seats"=>$seats,":fuelCapacity"=>$fuelCapacity,":maxspeed"=>$maxspeed,":additionalDesc"=>$additionalDesc,":img"=>$img));
+    
+        echo "Connection successfully";
+    
+}
+catch(PDOException $ex) {
+    echo "Datebase Connection failed: " . $ex->getMessage();
+}
+
+$conn = null;
+        
+
+?>         

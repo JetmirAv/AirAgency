cdf43f5c6fdd38014f4f6e7c70b8bcdd8ab1efdc
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-25 20:51:57 +0200

    user-table changes

--- dashboard/components/users-table.php
+++ dashboard/components/users-table.php
@@ -1,56 +1,50 @@
-<div class="card">
-
-
-	<?php include "../databaseConfig.php"?>
-	<?php
+<?php include "../databaseConfig.php"?>
+<?php
 	if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
 	$start_from = ($page-1) * 8;
 
-	$sql = "select fromCity,toCity,planeId,price,isSale,checkIn,createdAt,updatedAt from flight limit $start_from,8";
+	$sql = "select img , concat(firstname ,'  ', lastname) as fullname,gendre,email,birthday,state,city,phoneNumber from users limit $start_from,8";
 	$rs_result = $conn->query($sql);
 	$count = $rs_result->rowCount();
 		?>
 
-	<div class="header">
-		<h4 class="title">Number of flights:<?php echo "$count" ?></h4>
-	</div>
-	<div class="content table-responsive table-full-width">
-		<?php	
-	   echo '<table class="table table-hover table-striped">
-		    <thead>
-			 <th>Image</th>
-	   		 <th>From city </th>
-	   		 <th>To city </th>
-	   		 <th>Plane id </th>
-	   		 <th>Price</th>
-	   		 <th>Is sale</th>
-	   		 <th>Check in</th>
-	   		 <th>Created at</th>
-	   		 <th>Updated at</th>
-		    </thead>
-		 <tbody>';
-
+<div class="header">
+	<h4 class="title">Number of users:<?php echo "$count" ?></h4>
+</div>
+<div class="content table-responsive table-full-width">
+	<table class='table table-hover table-striped' style="table-layout: fixed;">
+		<thead>
+			<th width=3%>Image</th>
+			<th width=5%>Name</th>
+			<th width=3%>Gendre</th>
+			<th width=9%>Email</th>
+			<th width=5%>Birthday</th>
+			<th width=5%>State</th>
+			<th width=5%>City</th>
+			<th width=5%>Phone Number</th>
+		</thead>
+		<tbody>
+			<?php	
 
 	foreach($rs_result as $row){
 			echo' <tr>
-				<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-				<td>'.$row["fromCity"].'</td>
-				<td>'.$row["toCity"].'</td>
-				<td style="padding-left:25px;">'.$row["planeId"].'</td>
-				<td>'.$row["price"].'</td>
-				<td>'.$row["isSale"].'</td>
-				<td>'.$row["checkIn"].'</td>
-				<td>'.$row["createdAt"].'</td>
-				<td>'.$row["updatedAt"].'</td>
-			</tr>';
+					<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+					<td>'.$row["fullname"].'</td>
+					<td style="padding-left:25px;">'.$row["gendre"].'</td>
+					<td style="width:14% ; overflow:hidden;  position: relative;">'.$row["email"].'</td>
+					<td>'.$row["birthday"].'</td>
+					<td>'.$row["state"].'</td>
+					<td>'.$row["city"].'</td>
+					<th>'.$row["phoneNumber"].'</th>
+				</tr>';
 	}
 			
 		?>
 		</tbody>
-		</table>
+	</table>
 
-		<?php
-		$sql = "SELECT COUNT(id) AS total FROM flight";
+	<?php
+		$sql = "SELECT COUNT(id) AS total FROM users";
 		$result = $conn->query($sql);
 		$row = $result->fetch();
 		
@@ -60,12 +54,12 @@
         echo     "<div style='text-align: center'>";
 		for ($i=1; $i<=$total_pages; $i++)   // print links for all pages
 			{ 
-			echo "<a href='/github/AirAgency/dashboard/flights-table.php?page=".$i."' ";
+			echo "<a href='/github/AirAgency/dashboard/users-table.php?page=".$i."' ";
 			echo "style='margin-left:10px;'>"."      ".$i."</a> ";
 		    }; 
 		     
 		echo "</div>" 
 ?>
 
-	</div>
+</div>
 </div>

4544255526b3f5e208af7945caf36c5561365dc3
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-27 00:45:18 +0200

    flights table-show more data

--- dashboard/components/flights-table.php
+++ dashboard/components/flights-table.php
@@ -1,71 +1,86 @@
 <div class="card">
-
-
-	<?php include "../databaseConfig.php"?>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<?php include "../databaseConfig.php" ?>
 	<?php
-	if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
-	$start_from = ($page-1) * 8;
-
-	$sql = "select fromCity,toCity,planeId,price,isSale,checkIn,createdAt,updatedAt from flight limit $start_from,8";
+	$sql = "select id, fromCity,toCity,planeId,price,isSale,checkIn,createdAt,updatedAt from flight ORDER BY id asc limit 10 ";
 	$rs_result = $conn->query($sql);
-	$count = $rs_result->rowCount();
-		?>
+	$flight_id = '';
+	?>
 
 	<div class="header">
-		<h4 class="title">Number of flights:<?php echo "$count" ?></h4>
+		<h4 class="title">Number of flights:<?php echo $flight_id ?></h4>
 	</div>
 	<div class="content table-responsive table-full-width">
-		<?php	
-	   echo '<table class="table table-hover table-striped">
-		    <thead>
-			 <th>Image</th>
-	   		 <th>From city </th>
-	   		 <th>To city </th>
-	   		 <th>Plane id </th>
-	   		 <th>Price</th>
-	   		 <th>Is sale</th>
-	   		 <th>Check in</th>
-	   		 <th>Created at</th>
-	   		 <th>Updated at</th>
-		    </thead>
-		 <tbody>';
 
+		<table class="table table-hover table-striped">
 
-	foreach($rs_result as $row){
-			echo' <tr>
-				<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
-				<td>'.$row["fromCity"].'</td>
-				<td>'.$row["toCity"].'</td>
-				<td style="padding-left:25px;">'.$row["planeId"].'</td>
-				<td>'.$row["price"].'</td>
-				<td>'.$row["isSale"].'</td>
-				<td>'.$row["checkIn"].'</td>
-				<td>'.$row["createdAt"].'</td>
-				<td>'.$row["updatedAt"].'</td>
-			</tr>';
-	}
-			
-		?>
-		</tbody>
+			<thead>
+				<th>Image</th>
+				<th>From city </th>
+				<th>To city </th>
+				<th>Plane id </th>
+				<th>Price</th>
+				<th>Is sale</th>
+				<th>Check in</th>
+				<th>Created at</th>
+				<th>Updated at</th>
+			</thead>
+			<tbody id="load_data_table">
+				<?php
+				foreach ($rs_result as $row) {
+					echo '<tr>
+							<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+							<td>'.$row["fromCity"] .'</td>
+							<td>' . $row["toCity"] . '</td>
+							<td style="padding-left:25px;">' . $row["planeId"] . '</td>
+							<td>' . $row["price"] . '</td>
+							<td>' . $row["isSale"] . '</td>
+							<td>' . $row["checkIn"] . '</td>
+							<td>' . $row["createdAt"] . '</td>
+							<td>' . $row["updatedAt"] . '</td>
+						  </tr>';
+				}
+					$flight_id = $row["id"];
+				?>
+			</tbody>
+
 		</table>
-
-		<?php
-		$sql = "SELECT COUNT(id) AS total FROM flight";
-		$result = $conn->query($sql);
-		$row = $result->fetch();
-		
-		$a=$row['total']/8;
-		
-		$total_pages = ceil($a); // calculate total pages with results
-        echo     "<div style='text-align: center'>";
-		for ($i=1; $i<=$total_pages; $i++)   // print links for all pages
-			{ 
-			echo "<a href='/github/AirAgency/dashboard/flights-table.php?page=".$i."' ";
-			echo "style='margin-left:10px;'>"."      ".$i."</a> ";
-		    }; 
-		     
-		echo "</div>" 
-?>
-
+		<table id="first_row">
+			<tr id="remove_row">
+				<button type="button" name="btn_more" data-vid="<?php echo $flight_id; ?>" id="btn_more" class="btn btn-success form-control">more</button>
+			</tr>
+		</table>
 	</div>
 </div>
+
+
+<script>
+	$(document).ready(function() {
+		$(document).on('click', '#btn_more', function() {
+			var last_flight_id = $(this).data("vid");
+			$('#btn_more').html("Loading...");
+			$.ajax({
+				url: "components/loading-flights.php",
+				method: "POST",
+				data: {
+					last_flight_id: last_flight_id
+				},
+				dataType: "html",
+				success: function(data) {
+					if (data != '') {
+						$('#btn_more').remove();
+						$('#remove_row').remove();
+						$('#load_data_table').append(data);
+
+					} else {
+						$('#btn_more').html("No Data");
+					}
+				}
+			});
+		});
+	});
+
+</script>
---
+++ dashboard/components/loading-flights.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once "../../dbConfig.php";
+
+$dsn = "mysql:host=" . HOST . ";dbname=" . DBNAME;
+try {
+	$conn = new PDO($dsn, USERNAME, PASSWORD);
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
+
+$output = '';
+$flight_id = '';
+sleep(1);
+$data = (int)$_POST['last_flight_id'];
+
+$sql = "select id, fromCity,toCity,planeId,price,isSale,checkIn,createdAt,updatedAt from flight where id >" . $data . " order by id asc limit 10";
+$rs_result = $conn->query($sql);
+$count = $rs_result->rowCount();
+if ($count > 0) {
+	foreach ($rs_result as $row) {
+		$flight_id = $row['id'];
+		$output .= '
+                <tr>
+					<td style="padding:2px ; padding-left:10px"><img src="assets/img/faces/face-0.jpg" width=35;height=35; style="border-radius:50% ; padding:0px;"> </td>
+					<td>' . $row["fromCity"] . '</td>   
+					<td>' . $row["toCity"] . '</td>
+					<td style="padding-left:25px;">' . $row["planeId"] . '</td>
+	                <td>' . $row["price"] . '</td>
+	            	<td>' . $row["isSale"] . '</td>
+	            	<td>' . $row["checkIn"] . '</td>
+	            	<td>' . $row["createdAt"] . '</td>
+            		<td>' . $row["updatedAt"] . '</td>
+				</tr> ';
+	}
+	$output .= '
+		<tr id="remove_row">
+			<td colspan=9><button type="button" name="btn_more" data-vid="' . $flight_id . '" id="btn_more" class="btn btn-success form-control">more</button></td>
+		</tr>';
+
+	echo $output;
+}
+?>

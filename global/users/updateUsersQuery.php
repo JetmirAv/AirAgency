7ffa246385f0baa7f5b20dc254977a0342731ccf
jetmir.avdullahu@studentet.uni-pr.edu <jetmir.avdullahu@studentet.uni-pr.edu>
2019-05-29 18:11:41 +0200

    bla

--- global/users/updateUsersQuery.php
+++ global/users/updateUsersQuery.php
@@ -1,6 +1,11 @@
 <?php
 
 include "../../databaseConfig.php";
+include '../validations.php';
+
+session_start();
+$_SESSION['errors'] = '';
+$_SESSION['sucess'] = '';
 
 if (isset($_POST['userUpdate'])) {
     $firstname = $_POST['firstname'];
@@ -16,6 +21,25 @@ if (isset($_POST['userUpdate'])) {
     $img = $_POST['img'];
     $birthday = $_POST['birthday'];
     $number = $_POST['number'];
+
+    if (!valid_email($email)) {
+        $_SESSION['errors'] = 'Email is not valid!';
+    }
+    if ($password != '') {
+        if (!valid_password($password)) {
+            $_SESSION['errors'] = 'Password must have at least one Uppercase character,' .
+                ' one lowercase character, one number and be at least 8 characters long!';
+        } else {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+        }
+    }
+
+    if (!valid_phone($phoneNumber)) {
+        $_SESSION['errors'] = 'Phone number is not valid!';
+    }
+    if (!valide_date($birthday)) {
+        $_SESSION['errors'] = 'Birthday input is not valid!';
+    }
 
     $expireDate = $_POST['expireDate'];
     $expireDatesArray = explode('/', $expireDate);

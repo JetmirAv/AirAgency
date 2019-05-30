4c9da1e662913f2ae459dc6e7a0f9bf8988a602d
jetmir.avdullahu@studentet.uni-pr.edu <jetmir.avdullahu@studentet.uni-pr.edu>
2019-05-30 22:54:17 +0200

    index.php finished

--- main/components/sponsored.php
+++ main/components/sponsored.php
@@ -1,99 +1,53 @@
+<?php 
+
+$mostBookedDestinationsQuery = "
+select c.id, c.name from flight f 
+inner join city c
+on f.toCity=c.id
+where toCity in (
+select * from ( select f.toCity from booked b 
+inner join flight f on b.flightId = f.id
+group by f.toCity
+order by count(*) desc 
+limit 10) temp )
+group by c.id";
+$index = 1;
+$mostBookedDestinations = $conn->prepare($mostBookedDestinationsQuery);
+$mostBookedDestinations->execute();
+
+foreach($mostBookedDestinations->fetchAll() as $destinations):
+
+    // print_r($destinations);
+
+
+?>
+
+
 <!-- Single Project Slide -->
 <div class="single-project-slide active bg-img" style="background-image: url(img/bg-img/5.jpg);">
     <!-- Project Text -->
     <div class="project-content">
         <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
+            <h6>Destination <?php echo $index ?> </h6>
+            <h5><?php echo $destinations[1] ?></h5>
         </div>
     </div>
     <!-- Hover Effects -->
     <div class="hover-effects">
         <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-            <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+            <h6>Destination <?php echo $index ?></h6>
+            <h5><?php echo $destinations[1] ?></h5>
         </div>
-        <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+        <button onClick="bttnCityFlights('<?php echo $destinations[1] ?>')" class="btn project-btn">Book One Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
     </div>
 </div>
+<?php $index++; endforeach; ?>
 
-<!-- Single Project Slide -->
-<div class="single-project-slide bg-img" style="background-image: url(img/bg-img/6.jpg);">
-    <!-- Project Text -->
-    <div class="project-content">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-        </div>
-    </div>
-    <!-- Hover Effects -->
-    <div class="hover-effects">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-            <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
-        </div>
-        <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-    </div>
-</div>
 
-<!-- Single Project Slide -->
-<div class="single-project-slide bg-img" style="background-image: url(img/bg-img/7.jpg);">
-    <!-- Project Text -->
-    <div class="project-content">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-        </div>
-    </div>
-    <!-- Hover Effects -->
-    <div class="hover-effects">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-            <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
-        </div>
-        <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-    </div>
-</div>
+<script>
 
-<!-- Single Project Slide -->
-<div class="single-project-slide bg-img" style="background-image: url(img/bg-img/8.jpg);">
-    <!-- Project Text -->
-    <div class="project-content">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-        </div>
-    </div>
-    <!-- Hover Effects -->
-    <div class="hover-effects">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-            <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
-        </div>
-        <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-    </div>
-</div>
+function bttnCityFlights(city) {
+    window.location.href = "flights.php?city=" + city;
+}
 
-<!-- Single Project Slide -->
-<div class="single-project-slide bg-img" style="background-image: url(img/bg-img/9.jpg);">
-    <!-- Project Text -->
-    <div class="project-content">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-        </div>
-    </div>
-    <!-- Hover Effects -->
-    <div class="hover-effects">
-        <div class="text">
-            <h6>Entertaiment</h6>
-            <h5>Racing Bike</h5>
-            <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
-        </div>
-        <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-    </div>
-</div>
+</script>

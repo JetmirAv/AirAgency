757790b7a266809dec7fad53995fcea32e79ba86
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-31 23:22:30 +0200

    Flights book save

--- dashboard/components/insert/insertIntoBookingTable.php
+++ dashboard/components/insert/insertIntoBookingTable.php
@@ -1,15 +1,16 @@
 <?php
+session_start();
 include '../../../databaseConfig.php';
+include '../../../global/getDataJWT.php';
+include '../../../global/validations.php';
+
+
+$getToken=getDataJWT($_SESSION['token']);
+$userId=$getToken->id;
 
 $nrTickets=(int)$_POST['quantity'];
-$userId=(int)$_POST['userConectedId'];
 $flightId =(int) $_POST['flightId'];
 
-
-session_start();
-$_SESSION['InsertSucess'] = null;
-$_SESSION['insertQuantityError'] = null;
-$_SESSION['insertCardError'] = null;
 
 
 $countCards = "select  count(*) as count  from card where userId = ".$userId." ";
@@ -19,46 +20,56 @@ $count = $getStmForCards->fetch();
 $numberOfCards=$count['count'];
  
 
-$countAvalibleSeats = "select avalible from flight where id = ".$flightId." ";
+$countAvalibleSeats = "select avalible ,checkIn  from flight where id = ".$flightId." ";
 $getStmForAvalibleSeats = $conn->prepare($countAvalibleSeats);
 $getStmForAvalibleSeats -> execute();
 $seats = $getStmForAvalibleSeats->fetch();
 $numberOfSeatsAvaliable=$seats['avalible'];
+$bookDateFromDatabase=$seats['checkIn'];
 
-echo $numberOfSeatsAvaliable;
-//
-//if($numberOfCards>0){
-//       if($nrTickets>0){
-//		   if(){
-//       
-//       
-//       $getData = "select  price  from flight where id = ".$flightId." ";
-//       $getStm = $conn->prepare($getData);
-//       $getStm -> execute();
-//       $data = $getStm->fetch();
-//       
-//       
-//       $flightPrice=$data['price'];
-//       
-//       $insertBooked = "insert into booked (flightId,userId,price,quantity) values (:flightId,:userId,:price,:quantity);";
-//       $insertStatement = $conn->prepare($insertBooked);
-//       $pdoExecute = $insertStatement -> execute(array(":flightId"=>$flightId,":userId"=>$userId,":price"=>$flightPrice,":quantity"=>$nrTickets));
-//       	//$_SESSION['InsertSucess']= 
-//		   echo "You have bought ".$nrTickets." tickets";
-//       
-//		   }
-//		   else{
-//			   echo "You have bought ".$nrTickets." tickets";
-//		   }
-//		   }
-//       else{
-//       //	$_SESSION['insertQuantityError']= "Chose number of Tickets";
-//       echo "Chose number of Tickets";
-//	   }
-// }
-//else{
-////$_SESSION['insertCardError']= 
-//	echo "You don't have a card. Want to <a href='profile.php'>add</a> one";
-//}
+
+if( check_date($bookDateFromDatabase)){
+	//Check if number of tickets is larger than 0
+	if($nrTickets>0){
+
+		//Check if number of sets avalible is larger or equal than number of tickets 
+		if($nrTickets<=$numberOfSeatsAvaliable){
+
+			 //Check if user has card to pay
+			if($numberOfCards>0){
+				$getData = "select  price  from flight where id = ".$flightId." ";
+				$getStm = $conn->prepare($getData);
+				$getStm -> execute();
+				$data = $getStm->fetch();
+
+
+				$flightPrice=$data['price'];
+
+				$insertBooked = "insert into booked (flightId,userId,price,quantity) values (:flightId,:userId,:price,:quantity);";
+				$insertStatement = $conn->prepare($insertBooked);
+				$pdoExecute = $insertStatement -> execute(array(":flightId"=>$flightId,":userId"=>$userId,":price"=>$flightPrice,":quantity"=>$nrTickets));
+				
+				$update ="update  flight set  avalible=avalible - ".$nrTickets." where id=".$flightId."";
+				$updateAvalible = $conn->prepare($update);
+				$updateAvalible->execute();
+				
+				echo "You have bought ".$nrTickets." tickets    <br>Price: ".$nrTickets*$flightPrice."";
+			}
+			else{
+				echo "You don't have a card. Want to <a href='profile.php'>add</a> one";
+				}
+		}
+		else{
+			echo "We have only ".$numberOfSeatsAvaliable." seats avalible";
+		}
+	}
+	else{
+			echo "Chose number of tickets";
+			}
+}
+else{
+	echo "Can't buy a ticket of flight done";
+}
+			   
 
 ?>
--- main/flights.php
+++ main/flights.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 	<?php include 'components/head.php'?>
-
 <body>
 
 
@@ -71,7 +70,9 @@ if (isset($_GET['id'])) {
             where f.checkIn >= curdate() and f.checkIn >= current_time() and c2.name = '" . $_GET['city'] . "' order by f.checkIn desc limit 10 ;";
 
 } else {
-    $sql = "select c1.id as 'fromCityId',
+    $sql = "select 
+	            f.id as 'flightId',
+	            c1.id as 'fromCityId',
 				c2.id as 'toCityId',
 				c1.name as 'From',
 				c2.name as 'To',
@@ -94,7 +95,7 @@ if (isset($_GET['id'])) {
 //$countAllRows = "select count(*) as count from flight ";
 $rsResult = $conn->query($sql);
 //$rowCount = $conn->query($countAllRows);
-//$Count = $rowCount->fetch();
+$getFlightId = $rsResult->fetch();
 ?>
 
 
@@ -115,7 +116,7 @@ $rsResult = $conn->query($sql);
 		width: 100%;
 		height: 100%;
 		z-index: 300"></div>
-	<form action="components/delete/deleteUser.php" method="POST">
+	<form action="#" method="POST">
 		<div id="userDelete" style="
 		position: fixed;
 		background-color: #eee;
@@ -123,7 +124,7 @@ $rsResult = $conn->query($sql);
 		top: 30%;
 		left: 30%;
 		width: 40%;
-		height: 25%;
+		height: 40%;
 		z-index: 500;
 		text-align: center">
 			<h3>Book -></h3>
@@ -132,9 +133,34 @@ $rsResult = $conn->query($sql);
 				<div style="display:flex;
 					flex-direction: row;
 					justify-content: space-evenly ">
+					
 					<button id="bttnConfirmCancel">Cancel</button>
-					<button id="bttnConfirmBook" type="submit" name="deleteUser">book</button>
+					<button id="bttnConfirmBook" type="submit" name="deleteUser">Book</button>
 				</div>
+			
+
+		</div>
+	<form action="#" method="POST">
+		<div id="userInfo" style="
+		position: fixed;
+		background-color: #eee;
+		display: none;
+		top: 30%;
+		left: 30%;
+		width: 40%;
+		height: 30%;
+		z-index: 500;
+		text-align: center">
+			<h3>Result</h3>
+			<h4 style="font-weight: 300" id="userBook">
+				</h4>
+				<div style="display:flex;
+					flex-direction: row;
+					justify-content: space-evenly ">
+					<button id="btnConfirmCancel">Cancel</button>
+				</div>
+			
+
 		</div>
 	</form>
 	<!-- Breadcrumb Area Start -->
@@ -187,12 +213,18 @@ foreach ($rsResult as $row) {
                                 <h6>Date: <span>' . $row['Date'] . '</span></h6>
 						<h6>Time: <span>' . $row['Time'] . '</span></h6>
 						<h6>Avalible: <span id="ticketsAvalible">' . $row['Available'] . '</span></h6>
-						<span style="display:block; width: 20px; "><input min=0 max="100" type="number" id="quantityValue"/></span>
+						<span style="display:block; width: 20px; ">
+						<input min=0 max=' . $row['Available'] . ' onchange="test(this, \' ' . $row["flightId"] . '\')" type="number" class="'  . $row['flightId'] . '" />
+						</span>
+						
 					</div>
-					<a href="#" class="btn view-detail-btn">View Details <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+					<div style="display:flex">
+                    
+					<button class="btn btn-success form-control" style="width:20%; margin-left:100px" onclick="insertHandler(\' ' . $row["From"] . '\', \' ' . $row["To"] . '\', \' ' . $row["Price"] . '\', \' ' . $row["flightId"] . '\')" >Book now</button></div>
 				</div>
+
 			</div>
-                            ';}?>
+                           ';}?>
                             </div>
          <button type="button" id="bttnMore" class="btn btn-success form-control" style="width:160px; margin-left:48%; margin-bottom:20px; background-color:lightblue">Show More</button>
 
@@ -234,55 +266,70 @@ foreach ($rsResult as $row) {
     });
 
     let userInfo = document.getElementById("userDelete");
-	let backdrop = document.getElementById("backdrop");
-	let  From, To , Price= '';
-
-
-	function deleteHandler(From, To,Price) {
+	let backdrop = document.getElementById("backdrop"); 
+	let after = document.getElementById("userInfo"); 
+	let  From, To , Price , flightId, quantity= '';
+     let text=[]    
+	 
+	 function insertHandler(From, To,Price, flightId ) {
 		userInfo.style.display = "block";
 		backdrop.style.display = "block";
 		Price = Price;
 		From = From;
 		To = To;
-		//console.log("ID: " + id + " Name: " + From + " Email: " + To);
-		document.getElementById('user').innerHTML = " From: " + From + "   <br>   " + " To: " + To +
+		flightId:flightId;
+		window.value=flightId;
+	
+		 document.getElementById('user').innerHTML = " From: " + From + "   <br>   " + " To: " + To +
 			"<br>"+"Price:" + Price;
-
-		//alert(quantity);
-
+	
 	}
+	 
+	 
 
+	 
+	 
 	backdrop.onclick = () => {
 		userInfo.style.display = "none";
 		backdrop.style.display = "none";
 	}
-
-
-	document.getElementById("bttnConfirmCancel").onclick = (e) => {
-		e.preventDefault();
-		userInfo.style.display = "none";
-		backdrop.style.display = "none";
+ 
+	function test(e, id){
+		console.log(e.value + " " + id);
+		text[id] = e.value;
+		console.log(text);
 	}
+
 	document.getElementById("bttnConfirmBook").onclick = (e) => {
 		e.preventDefault();
-		var quantity = document.getElementById("quantityValue").value;
-		    var flightId=88;
-
+		
+		if (window.value in text){
+			var quantity= text[window.value];
+		}
+		else{
+			quantity=0;
+		}
+		
 				$.ajax({
 					url: "../dashboard/components/insert/insertIntoBookingTable.php",
 					method: "POST",
 					data: {
-						flightId: flightId,
+						flightId: window.value,
      	                quantity:quantity
 					},
 					dataType: "text",
 					success: function(data) {
-						$('#insertMessage').html(data);
-									userInfo.style.display = "none";
-									backdrop.style.display = "none";
+						console.log(data);
+						//$("#" + window.value ).html(data);
+						userInfo.style.display = "none";
+						//backdrop.style.display = "none";
+						after.style.display="block"
+				        document.getElementById('userBook').innerHTML =  data ;  	
+				   //document.getElementById('bttnConfirmBook').style.display = "none";  	
 
+               
 
-							}
+    		}
 
 
 
@@ -293,6 +340,17 @@ foreach ($rsResult as $row) {
 
 
 
+	}
+
+	document.getElementById("bttnConfirmCancel").onclick = (e) => {
+		e.preventDefault();
+		userInfo.style.display = "none";
+		backdrop.style.display = "none";
+	}
+	document.getElementById("btnConfirmCancel").onclick = (e) => {
+		e.preventDefault();
+		after.style.display = "none";
+		backdrop.style.display = "none";
 	}
 
 </script>

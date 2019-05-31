64d90256720f0a1e8201c46154a5090c160deeb6
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-05-31 23:32:44 +0200

    More Flight changes

--- main/moreFlights.php
+++ main/moreFlights.php
@@ -3,7 +3,9 @@ require_once("../databaseConfig.php");
 
 $output = "";
 $offset = (int)trim($_REQUEST['offset']);
-$lastBookedQuery = "select c1.id as 'fromCityId',
+$lastBookedQuery = "select
+			f.id as 'flightId',
+			c1.id as 'fromCityId',
             c2.id as 'toCityId',
             c1.name as 'From',
             c2.name as 'To',
@@ -42,10 +44,15 @@ if($count > 0){
                                 <h6>To: <span>'.$row['To'].'</span></h6>
                                 <h6>Date: <span>'.$row['Date'].'</span></h6>
 						<h6>Time: <span>'.$row['Time'].'</span></h6>
-						<h6>Avalible: <span id="ticketsAvalible">'.$row['Available'].'</span></h6>
-						<span style="display:block; width: 20px; "><input min=0 max="100" type="number" id="quantityValue"/></span>
+											<h6>Avalible: <span id="ticketsAvalible">' . $row['Available'] . '</span></h6>
+						<span style="display:block; width: 20px; ">
+						<input min=0 max=' . $row['Available'] . ' onchange="test(this, \' ' . $row["flightId"] . '\')" type="number" class="'  . $row['flightId'] . '" />
+						</span>
+						
 					</div>
-					<a href="#" class="btn view-detail-btn">View Details <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+					<div style="display:flex">
+                    
+					<button class="btn btn-success form-control" style="width:20%; margin-left:100px" onclick="insertHandler(\' ' . $row["From"] . '\', \' ' . $row["To"] . '\', \' ' . $row["Price"] . '\', \' ' . $row["flightId"] . '\')" >Book now</button></div>
 				</div>
 			</div>
                             ';

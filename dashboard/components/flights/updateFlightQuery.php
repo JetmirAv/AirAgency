38969c4c6fff9058a691f51c063d94730fc1cfd8
LaurentArifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-06-01 22:15:33 +0200

    update flight image validation

--- dashboard/components/flights/updateFlightQuery.php
+++ dashboard/components/flights/updateFlightQuery.php
@@ -1,87 +1,3 @@
-<?php
-//$servername = 'localhost';
-//$username = "root";
-//$password = "";
-//$database = 'airagency';
-//include "../../../databaseConfig.php";
-//
-//try{
-//   
-////    $conn = new PDO("mysql:host=localhost; dbname=airagency", $username, $password);
-////    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    
-//$errors = Array();
-//    
-//if(isset($_POST['updateFlight']))
-//{
-//    
-//    
-//    $fromCity =(int)$_POST['fromCity'];
-//    $toCity = (int)trim($_POST['toCity']);
-//    $planeId = (int)trim($_POST['planeId']);
-//    $avalible = (int)trim($_POST['avalible']);
-//    $price = floatval($_POST['price']);
-//    $isSale = (int)trim($_POST['isSale']);
-//    $checkIn = date(trim($_POST['checkIn']));
-//    $img = $_POST['img'];
-////    $updatedAt = $_POST['updatedAt'];
-// //   echo $toCity;
-//    
-//    $updateFlight = " Update flight set  fromCity ='$fromCity' , toCity = '$toCity', planeId = '$planeId', avalible = '$avalible', price = '$price', isSale = '$isSale', checkIn = '$checkIn' , img = '$img', updatedAt=NOW() where id = ".$flightId."    and ('$fromCity' in(select id from city ) and '$toCity' in (select id from city)); ";
-//    $updateStm = $conn->prepare($updateFlight);
-//    //print_r($updateFlight);
-//    $pdoExec = $updateStm->execute(array(":fromCity"=>$fromCity, ":toCity"=>$toCity, ":planeId"=>$planeId, ":avalible"=>$avalible, ":price"=>$price, ":isSale"=>$isSale, ":checkIn"=>$checkIn, ":img"=>$img));
-//    
-//    
-//    
-////    $updateFlight = " Update flight set  fromCity = '$fromCity, toCity = :toCity,planeId = :planeId, avalible = :avalible, price = :price, isSale = :isSale, checkIn = :checkIn, img = :img, updatedAt=NOW() where id = 101 and (fromCity in(select id from city where name=:fromCity) or toCity in (select id from city where name= :toCity)); ";
-//    
-//    
-//        //echo "Updated successfully";
-//    
-//  $checkInString='$checkIn';
-//  $checkInInt = (int)$checkInString;    
-//    
-//  echo $checkIn;
-//    if($checkInInt>0){
-//        if (!check_year($checkIn)) {
-//            $errmsg = "Invalid CheckIn";
-//        array_push($errors, $errmsg);
-//     }
-//        } else {
-//            $errmsg = "Negative number of date are not allowed";
-//            array_push($errors,$errmsg);
-//        }
-//    
-//    
-//    
-//        
-//        if($errors)
-//        {
-//            $msg = 'gabim';
-//            array_push($errors, $msg);
-//            $_POST['error'] = $errors;
-//        }
-//        else
-//        {
-//            header("location: ". $_SERVER["HTTP_REFERER"]);
-//            $_POST['sukses'] = "Sukses";
-//        }
-//    
-//}
-//    
-//}
-//catch(PDOException $ex) {
-//    echo "Datebase Connection failed: " . $ex->getMessage();
-//}
-//
-//$conn = null;
-//        
-
-?>  
-
-             
-              
    <?php
 
 include "../../../databaseConfig.php";
@@ -100,67 +16,103 @@ if(isset($_POST['updateFlight'])){
             $price = floatval($_POST['price']);
             $isSale = (int)trim($_POST['isSale']);
             $checkIn = date(trim($_POST['checkIn']));
-            $img = $_POST['img'];
+//            $img = $_POST['img'];
+	
+	
+			$_SESSION['sucess'] = "";
+			$_SESSION['errors'] = $error_message;
+			$allowed_image_extension = array(
+			"png",
+			"jpg",
+			"jpeg"
+			);
+
+	$doesExistFile = $_FILES["img"]["error"];
+
+print_r($doesExistFile);
+	
     
+if($doesExistFile === 0){
+$file_extension = pathinfo($_FILES["img"]["name"], PATHINFO_EXTENSION);
+// Validate file input to check if is not empty
+if (!file_exists($_FILES["img"]["tmp_name"])) {
+	$errmsg = "Choose image file to upload.";
+	array_push($error_message, $errmsg);
+}    // Validate file input to check if is with valid extension
+if (!in_array($file_extension, $allowed_image_extension)) {
+	$errmsg = "Upload valiid images. Only PNG and JPEG are allowed.";
+	array_push($error_message, $errmsg);
+}    // Validate image file size
+else if (($_FILES["img"]["size"] > 8000000)) {
+	$errmsg = "Image size exceeds 8MB";
+	array_push($error_message, $errmsg);
+}    // Validate image file dimensi..on
 
 
-			if(count($error_message)<=0){  
 
-                
-        $updateFlight = " Update flight set  fromCity ='$fromCity' , toCity = '$toCity', planeId = '$planeId', avalible = '$avalible', price = '$price', isSale = '$isSale', checkIn = '$checkIn' , img = '$img', updatedAt=NOW() where id = ".$flightId."    and ('$fromCity' in(select id from city ) and '$toCity' in (select id from city)); ";
-          $updateStm = $conn->prepare($updateFlight);
-     print_r($updateStm);
-                   $pdoExec = $updateStm->execute(array(":fromCity"=>$fromCity, ":toCity"=>$toCity, ":planeId"=>$planeId, ":avalible"=>$avalible, ":price"=>$price, ":isSale"=>$isSale, ":checkIn"=>$checkIn, ":img"=>$img));
+}
+// if($avalible<=0){ // $errmsg="Number of seats avalible can't be negative." ; // array_push($error_message, $errmsg); //} //if($price <=0){ // $errmsg="Number of price can't be negative." ; // array_push($error_message, $errmsg); //} //if($isSale <=0){ // $errmsg="Number of sale can't be negative." ; // array_push($error_message, $errmsg); //}
 
-				}
-				else {
-					$errormsg = "Fill all gaps please ! ";
-					array_push($error_message,$errmsgr);
-				 header('location: ' . $_SERVER["HTTP_REFERER"]);
-				die();
-				}
+	if(count($error_message)<=0){  
 
-			} 
+if($doesExistFile === 0){
 
-            else {
-				$_SESSION['errors'] = $error_message;
-			    header('location: ' . $_SERVER["HTTP_REFERER"]);
-				die();
-			}
+	$flightPic = $_FILES['img']['name'];
+	$expflightPic = explode('.', $flightPic);
+	$flightPicExptype = $expflightPic[1];
+	$date = date('m/d/Yh:i:sa', time());
+	$rand = rand(10000, 99999);
+	$encname = $date . $rand;
+	$flightPicName = md5($encname) . '.' . $flightPicExptype;
+	$flightPicPath = "../../../uploads/flight-img/" . $flightPicName;
+
+	if (move_uploaded_file($_FILES["img"]["tmp_name"], $flightPicPath)) { } else {
+		$errmsg = "Problem in uploading image files.";
+		array_push($error_message, $errmsg);
+	}
+
+}
+
+
+	if(count($error_message)<=0){  
+
+		$image = "select img  from flight where id = ? ";
+		$imgPrepare = $conn->prepare($image);
+		$imgPrepare -> execute([$flightId]);
+		$image = $imgPrepare->fetch();
+		$imageFetch=$image['img'];
+
+
+
+		$doesExistFile === 0 ? $params[":img"] = $flightPicName : $flightPicName=$imageFetch;
+
+		$updateFlight = " Update flight set fromCity ='$fromCity' , toCity = '$toCity', planeId = '$planeId', avalible = '$avalible', price = '$price', isSale = '$isSale', checkIn = '$checkIn' , img = '$flightPicName', updatedAt=NOW() where id = ".$flightId." and ('$fromCity' in(select id from city ) and '$toCity' in (select id from city)); ";
+		$updateStm = $conn->prepare($updateFlight);
+		print_r($updateStm);
+
+
+
+
+
+		$pdoExec = $updateStm->execute(array(":fromCity"=>$fromCity, ":toCity"=>$toCity, ":planeId"=>$planeId, ":avalible"=>$avalible, ":price"=>$price, ":isSale"=>$isSale, ":checkIn"=>$checkIn));
+		header('location: ' . $_SERVER["HTTP_REFERER"]);
+     	$errmsg = "Update sucessfully.";
+		array_push($error_message, $errmsg);
+
+	}
+		else {
+		$_SESSION['errors'] = $error_message;
+		header('location: ' . $_SERVER["HTTP_REFERER"]);
+		die();
+		}
+
+		}
 		
-		?>                     
-                
-                 
-                  
-                   
-                    
-                     
-                      
-                       
-                        
-                         
-                          
-                           
-                            
-                             
-                              
-                               
-                                
-                                 
-                                  
-                                   
-                                    
-                                     
-                                      
-                                       
-                                        
-                                         
-                                          
-                                           
-                                            
-                                             
-                                              
-                                               
-                                                
-                                                  
- 
+		else {
+		$_SESSION['errors'] = $error_message;
+		header('location: ' . $_SERVER["HTTP_REFERER"]);	
+		die();
+	}
+}
+		
+		?>
--- dashboard/components/update/flightsUpdate.php
+++ dashboard/components/update/flightsUpdate.php
@@ -1,17 +1,12 @@
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-    <?php 
+ <?php 
     include "../databaseConfig.php";
-
     // Query Flights
     //if (isset($_GET['id'])){
     //   $id = $_GET['id'];
     if (isset($_GET['id'])){
     $id = $_GET['id'];
-
-
-
-
     $sqlFlight="SELECT c1.id as 'fromCityId',
     a.id as 'airplaneId',
     a.name as 'AirplaneName',
@@ -41,235 +36,224 @@
     $flightDetail = $flightStatement->fetch(); 
     ?>
 
-    <?php 
+ <?php 
     //include "../databaseConfig.php";
     $allCities = "select id,name from city order by name ; ";
     $cityStm = $conn->prepare($allCities);
     $cityStm->execute();
     $results = $cityStm->fetchAll();
-
-
-
     ?>
 
-    <?php 
+ <?php 
     //include "../databaseConfig.php";
     $allPlanes = "select id,name from airplane order by name ; ";
     $planeStm = $conn->prepare($allPlanes);
     $planeStm->execute();
     $planeResults = $planeStm->fetchAll();
-
-    ?>           
-
+    ?>
 
 
-    <div class="col-md-8" style="margin-left:15%;">
-     <div class="card">
-            <div class="header">
-                <h4 class="title">Edit Flights</h4>
-            </div>
-            <div class="content">
-    <!--                                <form action="components/dashboard/insertFlight.php" method="post">-->
-                   <form action="../dashboard/components/flights/updateFlightQuery.php" method="POST" >
 
-          <div class="row" style="margin-left:300px;">
-            <div class="form-group" style="display: inline-block; margin-left: auto; margin-right:auto">
-        <!-- <label  id="inputlabel" for="form-img">Profile picture</label> -->
-        <input style="position: fixed; top:-100%; left: -100%" id="profileUpload" onchange="readURL(this)" type="file" name="img">
-        <img style="height:150px; width:150px;" id="profileImg" alt="profile" class="avatar"  src="../../AirAgency/uploads/flight-img/<?php echo $flightDetail['img'];?>" onclick="clicked(this)" />
-        </div>
-           </div>
+ <div class="col-md-8" style="margin-left:15%;">
+ 	<div class="card">
+ 		<div class="header">
+ 			<h4 class="title">Edit Flights</h4>
+ 		</div>
+ 		<div class="content">
+ 			<!--                                <form action="components/dashboard/insertFlight.php" method="post">-->
+ 			<form action="../dashboard/components/flights/updateFlightQuery.php" enctype="multipart/form-data" method="POST">
 
-                    <div class="row" style="margin-left:15%">
 
-                           <div class="col-md-5" >
-                            <div class="form-group" style="width: 250px;">
 
-                                <label>From City</label>      
-                                  <select name="fromCity"  class="btn btn-default dropdown-toggle">
+ 				<div class="row" style="margin-left:300px;">
+ 					<div class="form-group" style="display: inline-block; margin-left: auto; margin-right:auto">
+ 						<!-- <label  id="inputlabel" for="form-img">Profile picture</label> -->
 
-                                    <?php foreach($results as $output) 
+ 						<input style="position: fixed; top:-100%; left: -100%" id="profileUpload" onchange="readURL(this)" type="file" name="img" value="<?php echo $flightDetail['img'];?>">
+ 						<img style="height:150px; width:150px;" id="profileImg" alt="profile" class="avatar" src="../../AirAgency/uploads/flight-img/<?php echo $flightDetail['img'];?>" onclick="clicked(this)" />
+
+
+ 					</div>
+ 				</div>
+
+ 				<div class="row" style="margin-left:15%">
+
+ 					<div class="col-md-5">
+ 						<div class="form-group">
+
+ 							<label>From City</label>
+ 							<select name="fromCity" class="btn btn-default dropdown-toggle">
+
+ 								<?php foreach($results as $output) 
                                         {
                                            if($output['id']==$flightDetail['fromCity'])
                                            {
                                                ?>
 
-                                                <option value = "<?php echo $flightDetail['fromCityId'] ?>" selected >
-                                                        <?php  echo $flightDetail['From City']; ?>
-                                                    </option>
-                                           <?php 
+ 								<option value="<?php echo $flightDetail['fromCityId'] ?>" selected>
+ 									<?php  echo $flightDetail['From City']; ?>
+ 								</option>
+ 								<?php 
                                            }
-
                                             ?>
 
-                                            <option value = "<?php echo $output['id']?> selected">
-                                            <?php  echo $output['name']; ?></option>
+ 								<option value="<?php echo $output['id']?> selected">
+ 									<?php  echo $output['name']; ?></option>
 
-                                           <?php 
+ 								<?php 
                                             }
-
                                       ?>
 
-                                </select>
-                            </div>
-                        </div> 
+ 							</select>
+ 						</div>
+ 					</div>
 
-                              <div class="col-md-5">
-                            <div class="form-group" style="width: 250px;">
+ 					<div class="col-md-5">
+ 						<div class="form-group">
 
-                                <label>To City</label>      
-                                  <select name="toCity" class="btn btn-default dropdown-toggle">
-                                    <?php foreach($results as $output) 
+ 							<label>To City</label>
+ 							<select name="toCity" class="btn btn-default dropdown-toggle">
+ 								<?php foreach($results as $output) 
                                         {
                                            if($output['id']==$flightDetail['toCity'])
                                            {
                                                ?>
-                                               <option value = "<?php echo $flightDetail['toCityId']?> " selected >
-                                                        <?php  echo $flightDetail['To City']; ?>
-                                                </option>
-                                           <?php 
+ 								<option value="<?php echo $flightDetail['toCityId']?> " selected>
+ 									<?php  echo $flightDetail['To City']; ?>
+ 								</option>
+ 								<?php 
                                            }
-
                                             ?>
-                                            <option value = "<?php echo $output['id']?> selected ">
-                                            <?php  echo $output['name']; ?></option>
-                                           <?php 
+ 								<option value="<?php echo $output['id']?> selected ">
+ 									<?php  echo $output['name']; ?></option>
+ 								<?php 
                                         }
-
                                       ?>
 
-                                </select>
-                            </div>
-                        </div>
+ 							</select>
+ 						</div>
+ 					</div>
 
 
-                    </div>
+ 				</div>
 
-                    <div class="row" style="margin-left:35%">
+ 				<div class="row" style="margin-left:35%">
 
 
-                                 <div class="col-md-5"  >
-                            <div class="form-group" style="width: 250px;" >
+ 					<div class="col-md-5">
+ 						<div class="form-group" style="width: 250px;">
 
-                                <label>Airplane</label>      
-                                  <select name="planeId" class="btn btn-default dropdown-toggle">
-                                    <?php foreach($planeResults as $planeOutput) 
+ 							<label>Airplane</label>
+ 							<select name="planeId" class="btn btn-default dropdown-toggle">
+ 								<?php foreach($planeResults as $planeOutput) 
                                         {
                                            if($planeOutput['id']==$flightDetail['planeId'])
                                            {
                                                ?>
-                                                <option value = "<?php echo $flightDetail['airplaneId']?> " selected >
-                                                        <?php  echo $flightDetail['AirplaneName']; ?>
-                                                </option>
-                                           <?php 
+ 								<option value="<?php echo $flightDetail['airplaneId']?> " selected>
+ 									<?php  echo $flightDetail['AirplaneName']; ?>
+ 								</option>
+ 								<?php 
                                            }
-
                                             ?>
-                                            <option value = "<?php echo $planeOutput['id']?> selected ">
-                                            <?php  echo $planeOutput['name']; ?></option>
-                                           <?php 
+ 								<option value="<?php echo $planeOutput['id']?> selected ">
+ 									<?php  echo $planeOutput['name']; ?></option>
+ 								<?php 
                                         }
-
                                       ?>
 
-                                </select>
-                            </div>
-                        </div>
+ 							</select>
+ 						</div>
+ 					</div>
 
-                    </div>
-
-
-                    <div class="row" style="margin-left:10%">
-                             <div class="col-md-4">
-                            <div class="form-group">
-                                <label>CheckIn</label>l
-                                <input name="checkIn" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['CheckIn']; ?>">
-                            </div>
-                        </div>
+ 				</div>
 
 
-                             <div class="col-md-4" style="width: 80px;">
-                        <label style="padding-right:5px"> Available </label>
-                        <br>
-                        <input type="text" name="avalible" class="form-control" value="<?php echo $flightDetail['Available']; ?>">
-                        </div>
-
-                         <div class="col-md-4" style="width: 90px;">
-                       <label> isSale </label>
-                        <br>
-                        <input name="isSale" type="number" class="form-control" value="<?php echo $flightDetail['isSale']; ?>">
-                        </div>
+ 				<div class="row" style="margin-left:10%">
+ 					<div class="col-md-4">
+ 						<div class="form-group">
+ 							<label>CheckIn</label>l
+ 							<input name="checkIn" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['CheckIn']; ?>">
+ 						</div>
+ 					</div>
 
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label>Price</label>
-                                <input name="price" type="Text" class="form-control" placeholder="Price" value="<?php echo $flightDetail['Price']; ?>">
-                            </div>
-                        </div>
+ 					<div class="col-md-4" style="width: 80px;">
+ 						<label style="padding-right:5px"> Available </label>
+ 						<br>
+ 						<input type="text" name="avalible" class="form-control" value="<?php echo $flightDetail['Available']; ?>">
+ 					</div>
 
-                     </div>
-                     <div class="row" style="margin-left:20%;">
-
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label>Created at</label>
-                                <input disabled name="createdAt" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['created At'];?>">
-                            </div>
-                        </div>
+ 					<div class="col-md-4" style="width: 90px;">
+ 						<label> isSale </label>
+ 						<br>
+ 						<input name="isSale" type="number" class="form-control" value="<?php echo $flightDetail['isSale']; ?>">
+ 					</div>
 
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label>Updated at</label>
-                                <input disabled name="updatedAt" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['update At'];?>">
-                            </div>
-                        </div>
+ 					<div class="col-md-4">
+ 						<div class="form-group">
+ 							<label>Price</label>
+ 							<input name="price" type="Text" class="form-control" placeholder="Price" value="<?php echo $flightDetail['Price']; ?>">
+ 						</div>
+ 					</div>
 
-                     </div>
+ 				</div>
+ 				<div class="row" style="margin-left:20%;">
 
-                        <div class="row" style="margin-right:290px;">
-                    <button name="updateFlight" id="clickButton" value="<?php echo $id ?>" type="submit" class="btn btn-info btn-fill pull-right">Update Flight</button>
-                    <div class="clearfix" ></div>
-                </div>
+ 					<div class="col-md-4">
+ 						<div class="form-group">
+ 							<label>Created at</label>
+ 							<input disabled name="createdAt" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['created At'];?>">
+ 						</div>
+ 					</div>
 
 
-    <!--
+ 					<div class="col-md-4">
+ 						<div class="form-group">
+ 							<label>Updated at</label>
+ 							<input disabled name="updatedAt" type="datetime" class="form-control" placeholder="__/__/____" value="<?php echo $flightDetail['update At'];?>">
+ 						</div>
+ 					</div>
+
+ 				</div>
+
+ 				<div class="row" style="margin-right:290px;">
+ 					<button name="updateFlight" id="clickButton" value="<?php echo $id ?>" type="submit" class="btn btn-info btn-fill pull-right">Update Flight</button>
+ 					<div class="clearfix"></div>
+ 				</div>
+
+
+ 				<!--
                     <button name="updateFlight" type="submit" class="btn btn-info btn-fill pull-right">Update Flights</button>
                     <div class="clearfix"></div>
     -->
-                </form>
-            </div>
-        </div>
-    </div>
-    <?php      }
+ 			</form>
+ 		</div>
+ 	</div>
+ </div>
+ <?php      }
     else{
     echo "choose one row"; 
-
     }
-
     ?>
 
-    <script>
-    function readURL(input) {
-    if (input.files && input.files[0]) {
-    var reader = new FileReader();
+ <script>
+ 	function readURL(input) {
+ 		if (input.files && input.files[0]) {
+ 			var reader = new FileReader();
+ 			reader.onload = function(e) {
+ 				$('#profileImg')
+ 					.attr('src', e.target.result)
+ 					.width(150)
+ 					.height(150);
+ 			};
+ 			reader.readAsDataURL(input.files[0]);
+ 		}
+ 	}
 
-    reader.onload = function(e) {
-    $('#profileImg')
-    .attr('src', e.target.result)
-    .width(150)
-    .height(150);
-    };
+ 	function clicked() {
+ 		$("#profileUpload").click()
+ 	}
 
-    reader.readAsDataURL(input.files[0]);
-    }
-    }
-
-    function clicked() {
-    $("#profileUpload").click()
-    }
-    </script>                    
-
-
-
+ </script>

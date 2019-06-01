17b4aaa67ae797ab36a83a3b188ca2cd03ffbde4
jetmir.avdullahu@studentet.uni-pr.edu <jetmir.avdullahu@studentet.uni-pr.edu>
2019-06-01 02:36:00 +0200

    progress

--- main/components/flightSearch.php
+++ main/components/flightSearch.php
@@ -1,46 +1,71 @@
-<?php include "getCities.php"?>
-
+<script>
+    let executed = false;
+    function getCities(){
+        console.log("EXEc");
+        if(!executed){
+            $.ajax({
+                url: 'components/getCities.php',
+                type: 'POST',
+                success: function (data) {
+                    if (data != "") {
+                        document.getElementById("fromCity").innerHTML = data;
+                        document.getElementById("toCity").innerHTML = data;
+                    } else {
+                        alert("Jo")
+                        $("#to").append("<option >No Flight found</option>");
+                        $("#bttnFindFlight").attr('disabled', true);
+                    }
+                }
+            });
+            executed = true;
+        }
+    }
+    getCities();
+</script>
 <form action="#" method="post">
     <div class="row justify-content-between align-items-end">
         <div class="col-6 col-md-2 col-lg-3">
             <label for="Search">From</label>
-            <select onchange="getAssocCities(this)" name="fromCity" id="from" class="form-control">
+            <select
+                onchange="getAssocCities(this)"
+                name="fromCity"
+                id="fromCity"
+                class="form-control">
                 
-                <?php
-                    if(isset($_COOKIE['assocCity'])){
-                        echo $_COOKIE['assocCity'];
-                    } else {
-                        foreach($output as $key=>$foundCity){
-                            echo '<option value="'. $key .'">' . $foundCity . ' </option>';
-                        }
-                    }
-                ?>
-            </select>
+                </select>
         </div>
         <div id="test" class="col-6 col-md-2 col-lg-3">
             <label for="checkIn">To</label>
             <select name="to" id="toCity" class="form-control">
-                <?php
-                
-                foreach($output as $key=>$foundCity){
-                    echo '<option value="'. $key .'">' . $foundCity . ' </option>';
-                }
-                ?>
+            
             </select>
         </div>
         <div class="col-6 col-md-2 col-lg-3">
             <label for="checkOut">Check In</label>
-            <input type="date" min="<?php echo date("Y-m-d")?>" max="<?php echo date("Y-m-d", strtotime("+4 weeks")) ?>" class="form-control" id="checkOut" name="checkout-date">
+            <input
+                type="date"
+                min="<?php echo date("
+                Y-m-d") ?>"
+                max="<?php echo date("
+                Y-m-d", strtotime("
+                +4
+                weeks")) ?>"
+                class="form-control"
+                id="checkOut"
+                name="checkout-date">
         </div>
 
         <div class="col-12 col-md-3">
-            <button id="bttnFindFlight" type="submit" class="form-control btn roberto-btn w-100">Check Flights</button>
+            <button
+                id="bttnFindFlight"
+                type="submit"
+                class="form-control btn roberto-btn w-100">Check Flights</button>
         </div>
     </div>
 </form>
 
 <script>
- function getAssocCities(e) {
+    function getAssocCities(e) {
         console.log(e.value);
         $.ajax({
             url: 'components/getCities.php',
@@ -51,8 +76,9 @@
             },
             success: function (data) {
                 if (data != "") {
-                    document.cookie = "assocCity = " + data;
-                    location.reload();  
+                    alert(data);
+                    $("#toCity").empty();
+                    // document.getElementById("toCity").innerHTML = data;
                 } else {
                     alert("Jo")
                     $("#to").append("<option >No Flight found</option>");
@@ -61,5 +87,4 @@
             }
         });
     }
-
 </script>
--- main/components/getCities.php
+++ main/components/getCities.php
@@ -1,36 +1,31 @@
 <?php
-
+include("../../databaseConfig.php");
 $findFromCityQuery = "select id, name from city order by name";
 $output = [];
 // include_once('../../databaseConfig.php');
 
-if(isset($_POST['refreshCity'])){
+if (isset($_POST['refreshCity'])) {
 
-    include_once('../../databaseConfig.php');
+    include_once '../../databaseConfig.php';
 
     $findFromCityQuery = "
     select id, name from city where id in (
     select f.toCity from flight f
-    inner join city c on f.fromCity = c.id 
+    inner join city c on f.fromCity = c.id
     where f.fromCity=" . $_POST['fromCityId'] . "
     order by name)";
-} 
+}
 
 $findFromCity = $conn->prepare($findFromCityQuery);
 $findFromCity->execute();
 
-foreach($findFromCity as $city){
+foreach ($findFromCity as $city) {
     $output[$city[0]] = $city[1];
 }
 
-if(isset($_POST['refreshCity'])){
-    $options='';
-    foreach($output as $key=>$city){
-        $options .= '<option value="'. $key .'">' . $city . ' </option>';
-    }
-
-    echo $options;
+$options = '';
+foreach ($output as $key => $city) {
+    $options .= '<option value="' . $key . '">' . $city . ' </option>';
 }
 
-
-
+echo $options;
